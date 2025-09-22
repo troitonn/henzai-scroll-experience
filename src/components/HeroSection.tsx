fc5df0b277d503914c11ccf299396b3fa927654e
troitonn <150198515+troitonn@users.noreply.github.com>
2025-09-22 12:38:38 -0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection: React.FC = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{ 
-          backgroundImage: `url('/lovable-uploads/paineis-solares-com-por-do-sol-criados-usando-tecnologia-de-ia-generativa_772924-3791.jpg')` 
+          backgroundImage: `url('/lovable-uploads/solução-de-energia-digital.png')` 
         }}
       >
         <div className="absolute inset-0 bg-[#0F2D3A] bg-opacity-40"></div>
